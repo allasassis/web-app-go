2b9a004cc4d88407d5251c89dfe4b1404f5382ce
Allas Assis <allasassis10@gmail.com>
2023-10-15 19:01:15 -0300

    Creating the TR using the Go range function in html

--- main.go
+++ main.go
@@ -5,6 +5,13 @@ import (
 	"net/http"
 )
 
+type Product struct {
+	Name        string
+	Description string
+	Price       float64
+	Quantity    int
+}
+
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
@@ -13,5 +20,12 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	temp.ExecuteTemplate(w, "Index", nil)
+	products := []Product{
+		{"Sneakers", "Green", 59.99, 293},
+		{"Sports Cap", "Black", 14.99, 150},
+		{"Denim Jeans", "Blue", 49.99, 250},
+		{"Neon Green Sneakers", "Green", 59.99, 100},
+		{"Summer Yellow Dress", "Yellow", 34.99, 180},
+	}
+	temp.ExecuteTemplate(w, "Index", products)
 }
